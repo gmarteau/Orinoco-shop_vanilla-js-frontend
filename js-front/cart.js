e98d03cb95c101db21281c634217486073c50162
Guillaume Marteau <guillaumemarteau@hotmail.fr>
2021-03-03 18:54:44 +0100

    v0.4.0: pages panier fonctionnelle, version repsonsive faite

--- js-front/cart.js
+++ js-front/cart.js
@@ -1,6 +1,75 @@
+/* Affichage du panier
+***********************************************************/
+
+const createCartFromLocalStorage = () => {
+    const cartItemTemplate = document.querySelector("#cartItem");
+    const cartList = document.querySelector("#cartList");
+    let productsListString = localStorage.getItem("productsInCart");
+    let productsList = JSON.parse(productsListString);
+    productsList.forEach((product) => {
+        let productObject = JSON.parse(product);
+        let clone = document.importNode(cartItemTemplate.content, true);
+        clone.querySelector(".cart__item__pic__img").setAttribute("src", productObject.imgUrl);
+        clone.querySelector(".cart__item__pic__img").setAttribute("alt", "Peluche " + productObject.name);
+        clone.querySelector(".cart__item__txt__name").innerHTML = productObject.name;
+        clone.querySelector(".cart__item__txt__quantity").innerHTML = "x" + productObject.quantity;
+        clone.querySelector(".cart__item__price").innerHTML = productObject.totalPrice + "€";
+        clone.querySelector(".btn--cross").setAttribute("id", productObject.productId);
+        cartList.appendChild(clone);
+    });
+};
+
+createCartFromLocalStorage();
+
+const calculateTotalPrice = () => {
+    const totalPriceHTML = document.querySelector(".cart__totalPrice");
+    const itemPrices = document.querySelectorAll(".cart__item__price");
+    let pricesAsNumbers = [];
+    itemPrices.forEach((price) => {
+        let priceString = price.textContent;
+        let priceNumber = parseInt(priceString, 10);
+        pricesAsNumbers.push(priceNumber);
+    });
+    let totalPrice = pricesAsNumbers.reduce((a, b)=> a + b,0);
+    console.log(totalPrice);
+    totalPriceHTML.innerHTML = "Total: " + totalPrice + "€"
+};
+
+calculateTotalPrice();
+
+
+const removeItemButtons = document.querySelectorAll(".btn--cross");
+console.log(removeItemButtons);
+
+// Chaque bouton permettant de supprimer un item a pour id l'id de l'item, donc si un bouton est cliqué, on compare son id aux ids des objets contenus dans le localStorage,
+// et on supprime du localStorage l'objet correspondant avant de recharger la page qui ne contient désormais plus l'item
+Array.from(removeItemButtons).forEach((button) => {
+    button.addEventListener("click", function () {
+        let itemsInStorageString = localStorage.getItem("productsInCart");
+        let itemsInStorage = JSON.parse(itemsInStorageString);
+        itemsInStorage.forEach((item) => {
+            let itemObject = JSON.parse(item);
+            let itemIndex = itemsInStorage.indexOf(item);
+            if (itemObject.productId == button.id) {
+                itemsInStorage.splice(itemIndex, 1);
+                localStorage.setItem("productsInCart", JSON.stringify(itemsInStorage));
+                console.log(localStorage);
+                document.location.reload();
+            };
+        });    
+    });
+});
+
+// removeItemButtons.addEventListener("click", function () {
+//     let buttonId = this.id;
+//     console.log(buttonId);
+// });
+
+/* Envoi de la commande au serveur
+***********************************************************/
+
 const apiUrl = "http://localhost:3000/api/teddies";
 const sendOrderButton = document.querySelector("#placeOrder");
-
 
 // Récupère les données saisies par l'utilisateur dans les champs du formulaire, puis les regroupe dans un objet "contact" qui est retourné
 const createContact = () => {
@@ -22,13 +91,14 @@ const createContact = () => {
 // Récupère les IDs des produits du panier en allant les chercher dans les objets présents dans le localStorage
 const createIDsArray = () => {
     let productsListString = localStorage.getItem("productsInCart");
-    console.log(productsListString);
+    //console.log(productsListString);
     let productsList = JSON.parse(productsListString);
-    console.log(productsList);
+    //onsole.log(productsList);
     let IDsArray = [];
     productsList.forEach((product) => {
-        IDsArray.push(product.productId);
-        console.log(product.productId);
+        let productObject = JSON.parse(product);
+        IDsArray.push(productObject.productId);
+        //console.log(productObject.productId);
     })
     console.log(IDsArray);
     return IDsArray;
--- js-front/product-page.js
+++ js-front/product-page.js
@@ -126,4 +126,3 @@ const addToCartButton = document.querySelector("#addToCart");
 addToCartButton.addEventListener("click", addProductToCart);
 
 // localStorage.clear();
-// console.log(localStorage.getItem("productsInCart"));
